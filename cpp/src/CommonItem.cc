4c69304d872ae734dd50a5c5fd289c5e69980748
Fabian Eisele <eisele@posteo.de>
2021-04-03 16:16:31 +0200

    correction in CommonItem: namespace for constructor.

--- cpp/src/CommonItem.cc
+++ cpp/src/CommonItem.cc
@@ -1,5 +1,5 @@
 #include "CommonItem.h"
 
-Common_item::CommonItem(const std::string& name, int sellIn, int quality) : Meta_item(name, sellIn, quality)
+CommonItem::CommonItem(const std::string& name, int sellIn, int quality) : Meta_item(name, sellIn, quality)
 {
 }
